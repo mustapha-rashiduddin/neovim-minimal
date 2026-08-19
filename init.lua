28ef1e1979722123e1a2152535b61b6090131675
mustapha-rashiduddin <chumchumchira@gmail.com>
2026-08-19 18:30:47 +0500

    remember last cursor point

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termbidi = true
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function(event)
+    local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(event.buf) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
